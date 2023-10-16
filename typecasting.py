d9ce201e8af2566e2d258cc196f9e9110ed03c5c
Amjadali168 <amjadali1682002@gmail.com>
2023-10-16 14:10:39 +0530

    typecasting

--- typecasting.py
+++ typecasting.py
@@ -1,6 +1,8 @@
 import numpy as np
 a = np.array([1,2,3,40,5])
 b = np.array([10,20,30,40])
+# a = a.astype('float32')
+a = a.astype('complex128')
 print(a)
 print(a.dtype)
-print(b.dtype)
+# print(b.dtype)
